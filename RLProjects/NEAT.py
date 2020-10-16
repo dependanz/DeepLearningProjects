53977524df8af17e3884680912b2dfafd61a95a4
deziox <danzel.serrano@gmail.com>
2020-10-15 21:33:38 -0400

    Python Implementation of NEAT

---
+++ RLProjects/NEAT.py
@@ -0,0 +1,129 @@
+import random
+
+'''
+  Connection Gene Class
+'''
+
+
+class Gene:
+    def __init__(self, i=0, o=0, e=True, innov=0):
+        self.input = i
+        self.output = o
+        self.enabled = e
+        self.innovation = innov
+        self.weight = random.randint(-100, 100) * 0.01
+
+    def set_link(self, o):
+        self.output = o
+
+    def set_enabled(self, e):
+        self.enabled = e
+
+    def __str__(self):
+        return '[' + str(self.input) + "] -> [" + str(self.output) + ']\ninnovation: ' + str(self.innovation) + '\n'
+
+
+'''
+  Neuron Node Class
+  
+  do i need this? for now just using dictionary
+'''
+
+
+class Node:
+    def __init__(self, i=0, l=0):
+        self.id = i
+        self.layer = l
+
+
+'''
+  Genome Class
+'''
+
+
+class Genome:
+    def __init__(self, n_x, n_y):
+
+        # list of genes
+        self.connection_genes = list()
+
+        # dictionary of nodes in our network (phenotype) (int node, int layer)
+        self.node_genes = {}
+
+        for i in range(n_x):
+            self.node_genes[i] = 0
+
+        for j in range(n_x, n_x + n_y):
+            self.node_genes[j] = 2
+
+        print(len(self.node_genes))
+
+    def add_gene(self, input, output, innovation_manager):
+        i_n = len(innovation_manager.innovation_numbers)
+        g = Gene(input, output, True, i_n)
+        if (innovation_manager.check_gene(g)):
+            innovation_manager.add_gene(g)
+            self.connection_genes.append(g)
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        ret = ""
+        for i in self.connection_genes:
+            ret += str(i)
+        return ret
+
+
+'''
+  I don't believe i need this
+'''
+
+
+class InnovationManager:
+
+    def __init__(self):
+        # tuple of innovation numbers with connection Gene
+        self.innovation_numbers = list()
+
+    def check_gene(self, g):
+        # valid gene:
+        #   check if connection pair exists in tuple pairing
+        for i in self.innovation_numbers:
+            if (i[1].input == g.input and i[1].output == g.output):
+                if (i[0] != g.innovation):
+                    return False
+            if(i[0] == g.innovation):
+                if (i[1].input != g.input or i[1].output != g.output):
+                    return False
+        return True
+
+    def add_gene(self,g):
+        self.innovation_numbers.append((len(self.innovation_numbers),g))
+
+
+'''
+  Connection Mutation
+'''
+def connection_mutation(genome, innovation_manager):
+    connection_genes = genome.connection_genes
+    node_genes = genome.node_genes
+    in_node = random.randint(0, len(node_genes)-1)
+    out_node = random.randint(0, len(node_genes)-1)
+    print(in_node,out_node)
+    while (in_node == out_node) or (node_genes[in_node] == 0 and node_genes[out_node] == 0):
+        out_node = random.randint(0, len(node_genes)-1)
+
+    while not genome.add_gene(in_node,out_node,innovation_manager):
+      in_node = random.randint(0, len(node_genes)-1)
+      while (in_node == out_node) or (node_genes[in_node] == 0 and node_genes[out_node] == 0):
+        out_node = random.randint(0, len(node_genes)-1)
+
+'''
+  Node Mutation
+'''
+
+
+def node_mutation(genome,innovation_manager):
+    connection_genes = genome.connection_genes
+    node_genes = genome.node_genes
---
+++ RLProjects/NeatTest1.py
@@ -0,0 +1,18 @@
+from RLProjects.NEAT import Gene, Genome, InnovationManager, connection_mutation, node_mutation
+
+# before doing the galaga project I wanna understand NEAT
+genome = Genome(3, 1)
+innovationManager = InnovationManager()
+
+genome.add_gene(0, 3, innovationManager)
+
+print(genome)
+
+connection_mutation(genome,innovationManager)
+
+print(genome)
+
+for i in range(3):
+    connection_mutation(genome,innovationManager)
+
+print(genome)
